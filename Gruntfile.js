b53fb46c668dde462d5db27629c5915302cb5aac
HWangJJUng <marvel94@nhnnext.org>
2015-04-05 19:10:35 +0900

    modify .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+// Gruntfile.js
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+    
+        }
+    });
+   
+    // Load the plugin that provides the "uglify" task.
+
+    // Default task(s).
+    grunt.registerTask('default', []);
+};
+
+
