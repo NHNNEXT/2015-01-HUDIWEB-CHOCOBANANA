8f7e6ec33b456ef378954bba0b04839fcaf1e475
HWangJJUng <marvel94@nhnnext.org>
2015-04-13 14:35:52 +0900

    Complete Validation

--- webapp/commons/js/start/signup_validate.js
+++ webapp/commons/js/start/signup_validate.js
@@ -25,53 +25,64 @@ ubuntudo.ui.validateManager = (function () {
 		EXIST_EMAIL: "이미 사용중인 이메일이다요.",
 		DEFAULT: "필수 정보입니다.",
 		WARNING_PASSWORD: "8자이상의 영문 대문자,소문자,숫자,특수문자를 사용하세요.",
-		GOOD_PASSWORD: "아름다운 비밀번호다요!"
+		GOOD_PASSWORD: "아름다운 비밀번호다요!",
+		CONFIRM_PASSWORD: "비밀번호가 일치합니다."
 	}
 
 	var util = ubuntudo.utility;
 
 	function ValidateManager(elBase) {
 		if (this instanceof ValidateManager) {
-			this.elEmail = elBase.getElementById(IDNAME.EMAIL);
-			this.elPassword = elBase.getElementById(IDNAME.PASSWORD);
-			this.elPassword2 = elBase.getElementById(IDNAME.PASSWORD_CHECK);
-			this.elForm = elBase.getElementById(IDNAME.SIGNUP_FORM);
+			this.elEmail = document.getElementById(IDNAME.EMAIL);
+			this.elPassword = document.getElementById(IDNAME.PASSWORD);
+			this.elPassword2 = document.getElementById(IDNAME.PASSWORD_CHECK);
+			this.elForm = document.getElementById(IDNAME.SIGNUP_FORM);
 		} else {
 			return new ValidateManager(elBase);
 		}
 	}
 
 	ValidateManager.prototype.validateForms = function () {
-
+		var inputTimer = null;
 		this.elForm.addEventListener("keyup", function (e) {
 
-			if(e.currentTarget === this.elEmail) {
-				this.validateEmail();
-			}
-			else if(e.currentTarget === this.elPassword) {
-				this.validatePassword();
-			}
-			else if(e.currentTarget === this.elPassword2) {
-				this.passwordCheck();
+			if (CheckKeyCode(e)) {
+				if (inputTimer !== null) {
+					clearTimeout(inputTimer);
+				}
+				inputTimer = setTimeout(function () {
+					if(e.target.id === IDNAME.EMAIL) {
+						this.validateEmail();
+					}
+					else if(e.target.id === IDNAME.PASSWORD) {
+						this.validatePassword();
+					}
+					else if(e.target.id === IDNAME.PASSWORD_CHECK) {
+						this.validatePassword2();
+					}
+				}.bind(this), 500);
 			}
 		}.bind(this));
 	}
 
+	//ValidateManager.prototype.preventTooManyEvent = function (e , callback) {
+	//	var inputTimer = null;
+	//	if (CheckKeyCode(e)) {
+	//		if (inputTimer !== null) {
+	//			clearTimeout(inputTimer);
+	//		}
+	//		inputTimer = setTimeout(function () {
+	//			callback();
+	//		}, 500);
+	//	}
+	//}
+
 	ValidateManager.prototype.validateEmail = function () {
-		var elEmail = document.getElementById(IDNAME.EMAIL);
 		var emailInputTimer = null;
 		var elMsg = document.getElementById(IDNAME.EMAIL_MESSAGE);
+		var elEmail = this.elEmail;
 
-		elEmail.addEventListener("keyup", function (e) {
-			if (emailInputTimer !== null) {
-				clearTimeout(emailInputTimer);
-			}
-			emailInputTimer = setTimeout(function () {
-				_checkRegexpEmail(e);
-			}, 500);
-		});
-
-		function _checkRegexpEmail(e) {
+		function _checkRegexpEmail() {
 			var email = elEmail.value;
 			var isEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			var isHan = /[ㄱ-ㅎ가-힣]/g;
@@ -80,16 +91,14 @@ ubuntudo.ui.validateManager = (function () {
 				elMsg.className = CLASSNAME.ERROR;
 				elMsg.innerHTML = MESSAGE.RECHECK_EMAIL;
 			} else {
-				_ajaxSearchEmail(e)
+				_ajaxSearchEmail();
 			}
 		}
 
-		function _ajaxSearchEmail(e) {
-			if (CheckKeyCode(e)) {
-				emailInputTimer = null;
-				var email = "email=" + elEmail.value;
-				util.postJSONData("/validate.do", email, _showValidationMessage);
-			}
+		function _ajaxSearchEmail() {
+			emailInputTimer = null;
+			var email = "email=" + elEmail.value;
+			util.postJSONData("/validate.do", email, _showValidationMessage);
 		}
 
 		function _showValidationMessage(ajaxResult) {
@@ -104,8 +113,12 @@ ubuntudo.ui.validateManager = (function () {
 				}
 				elMsg.style.display = "block";
 			} else {
+
 			}
 		}
+
+		_checkRegexpEmail();
+
 	}
 
 	ValidateManager.prototype.validatePassword = function () {
@@ -129,8 +142,6 @@ ubuntudo.ui.validateManager = (function () {
 		}
 
 		function _checkPassword1() {
-			checkPassword2("check");
-
 			var pw = document.getElementById(IDNAME.PASSWORD).value;
 			var elMsg = document.getElementById(IDNAME.PASSWORD_MESSAGE);
 			if (pw === "") {
@@ -147,37 +158,35 @@ ubuntudo.ui.validateManager = (function () {
 			elMsg.style.display = "block";
 		}
 
-		function checkPassword2(event) {
+		_checkPassword1();
+	}
+
+	ValidateManager.prototype.validatePassword2 = function () {
+		function checkPassword2() {
 			var password1 = document.getElementById(IDNAME.PASSWORD).value;
 			var password2 = document.getElementById(IDNAME.PASSWORD_CHECK).value;
 			var elMsg = document.getElementById(IDNAME.PASSWORD_CHECK_MESSAGE);
 
 			if (password2 == "") {
-				oImg.className = "ps_box int_pass_check";
-				elMsg.style.display = "block";
-				elMsg.innerHTML = "필수 정보입니다.";
-				return false;
+				elMsg.className = CLASSNAME.ERROR;
+				elMsg.innerHTML = MESSAGE.DEFAULT;
 			}
 			if (password1 != password2) {
-				oImg.className = "ps_box int_pass_check";
-				elMsg.style.display = "block";
+				elMsg.className = CLASSNAME.ERROR;
 				elMsg.innerHTML = "비밀번호가 일치하지 않습니다.";
-				document.getElementById("password2").value = "";
-				return false;
 			} else {
-				oImg.className = "ps_box int_pass_check2";
-				elMsg.style.display = "none";
-				return true;
+				elMsg.className = CLASSNAME.OK;
+				elMsg.innerHTML = MESSAGE.CONFIRM_PASSWORD;
 			}
-
-			return true;
+			elMsg.style.display = "block";
 		}
+		checkPassword2();
 	}
 
 
 	function CheckKeyCode(e) {
 		var KeyCode = 0;
-		if (window.event) { // IE
+		if (window.event) { // IE Chrome Safari
 			KeyCode = e.keyCode;
 		} else if (e.which) { // netscape ff opera
 			KeyCode = e.which;
--- webapp/commons/js/start/ubuntudo_main.js
+++ webapp/commons/js/start/ubuntudo_main.js
@@ -11,7 +11,7 @@ window.addEventListener("load", function () {
 
 	var elTarget = document.querySelector(".signup_box");
 	var validateManager = new ubuntudo.ui.validateManager(elTarget);
-	validateManager.validateEmail();
+	validateManager.validateForms();
 
 });
 
