a03e431873e778053f52fa3ef2d212a0208b2a57
HELLO <helllo@HELLOui-MacBook-Pro.local>
2015-04-21 13:29:03 +0900

    add todo modal ui #39

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,34 @@
 // Gruntfile.js
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-    
+
         }
     });
-   
+
     // Load the plugin that provides the "uglify" task.
 
     // Default task(s).
     grunt.registerTask('default', []);
+
 };
 
+function name(sdfsdf) {
+    sdjkflsdfj
+}
 
+function function_name(argument) {
+    fsf
+}
+
+function fdf(argument) {
+    // body...
+}
+
+chang.prototype.changme = function(changpara) {
+    changzzang
+};
--- webapp/commons/js/add todo modal/todo modal.js
+++ webapp/commons/js/add todo modal/todo modal.js
@@ -1,33 +1,4 @@
-// jQuery(document).ready(function(){
-    
-//     var select = $("select#color");
-    
-//     select.change(function(){
-//         var select_name = $(this).children("option:selected").text();
-//         $(this).siblings("label").text(select_name);
-//     });
-// });
-
-
-// function jsFunction(){
-//   var myselect = document.getElementById("selectOpt");
-//   alert(myselect.options[myselect.selectedIndex].value);
-// }
-
 function showSelectedParty() {
     var partyName = document.getElementById("select_party_list").value;
     document.getElementById("selected_party_name").innerHTML = partyName;
 }
-
-function showSelectedMonth() {
-    var date = document.getElementsByClassName("select_month_list")[0].value;
-    document.getElementById("selected_month").innerHTML = date;
-}
-
-function showSelectedDay() {
-    var date = document.getElementsByClassName("select_day_list")[0].value;
-    document.getElementById("selected_day").innerHTML = date;
-}
-
-// <label id="selected_date"></label>
-//onchange="showSelectedDate()"
