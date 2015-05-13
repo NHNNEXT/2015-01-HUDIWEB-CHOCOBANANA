5e272dfdd6927ee146335edb319137ee91184c5d
Mark K <k.mark.kr@gmail.com>
2015-05-13 14:31:14 +0900

    close #95

--- src/test/controller/GuildColtrollerTest.java
+++ src/test/controller/GuildColtrollerTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import ubuntudo.controller.guild.GuildController;
+import ubuntudo.controller.GuildController;
 import ubuntudo.model.GuildEntity;
 
 import com.google.gson.Gson;
