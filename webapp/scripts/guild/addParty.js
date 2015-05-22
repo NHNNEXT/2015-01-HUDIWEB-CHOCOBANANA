15f89089ee59cdf88f02a11433f24df7b7a9ad4b
030ii <lovelygeee2@gmail.com>
2015-05-22 11:29:08 +0900

    guild페이지에서 add party하기

--- webapp/scripts/guild/addParty.js
+++ webapp/scripts/guild/addParty.js
@@ -1,20 +1,50 @@
+/*
+ * ubuntudo = {};
+ * ubuntudo.utility = {};
+ */
 
-// 파티 추가하기 버튼을 눌렀을 때
-document.getElementById("party_add_btn").addEventListener("click", addParty);
+// 파티 추가하기 박스를 눌렀을 때
+document.getElementById("party_add_box_btn").addEventListener("click", inputPartyName);
 
+//파티 추가하기 박스에서, 추가 버튼을 눌렀을 때
+document.getElementById("inputPartyName_add_btn").addEventListener("click", addParty);
 
-// 사각형을 추가하는 함수
+//파티 추가하기 박스에서, 취소 버튼을 눌렀을 때
+document.getElementById("inputPartyName_cancel_btn").addEventListener("click", originalPartyAddBox);
+
+function inputPartyName(){
+	document.getElementById("party_add_box_btn").style.display = "none";
+	document.getElementById("partyName_input_box_btn").style.display = "block";	
+}
+
 function addParty(){
-  var newParty = document.createElement("div");
+	var partyName = document.getElementsByClassName("input_partyName")[0].value;
+	var newParty = document.createElement("div");
+	var newPartyName = document.createElement("div");
+	var text = document.createTextNode(partyName);
   
-  newParty.className = "party_list";
-  
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    
-  // 임의의 색상 넣기
-  newParty.style.backgroundColor ='#' + randomColor;
- 
-  
-  // board에 사각형 추가
-  document.getElementsByClassName("party_list_wrap")[0].appendChild(newParty);
+	// css 추가
+	newParty.className = "party_list";
+	newPartyName.className = "party_name";
+	
+	// 파티 목록에 파티 추가
+	document.getElementsByClassName("added_party")[0].appendChild(newParty);
+	newParty.appendChild(newPartyName);
+	newPartyName.appendChild(text);
+	
+	 /* pid, p_name, party_leader_id도 함께 추가해야함 */
+	/*
+	 * pid++
+	 * p_name = partyName
+	 * party_leader = 현재 로그인한 유저
+	 * 
+	 */
+	
+	originalPartyAddBox();
+}
+
+function originalPartyAddBox() {
+	document.getElementsByClassName("input_partyName")[0].value = "";
+	document.getElementById("party_add_box_btn").style.display = "block";
+	document.getElementById("partyName_input_box_btn").style.display = "none";
 }
--- webapp/scripts/guild/ubuntudoGuild.js
+++ webapp/scripts/guild/ubuntudoGuild.js
@@ -1,6 +1,26 @@
+/*
+retrieveGuildDetailAndPartyListController()
+
+// 길드 헤더에서
+길드 이름 = result[0].g_name
+리더 = result[0].leader_id
+멤버수 = result[0].members
+파티수 = result[0].parties
+
+// 파티목록에서 (파티 목록 나열은 pid순으로??)
+파티이름 = result[1].p_name
+
+// 파티 추가할때 
+파티이름으로 입력한 값을 p_name에 저장해야함.
+pid도 같이 새로 추가해야
+이 파티를 만든 사람 아이디 저장해야 (로그인되어있는 user)
+
+*/
+
 
 ubuntudo = {};
 ubuntudo.utility = {};
+
 (function () {
     'use strict';
     var joinBtn = document.querySelector(".guild_join_btn");
