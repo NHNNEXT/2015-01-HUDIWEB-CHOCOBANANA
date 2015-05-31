e0c2a8ab51987917df79174f395158debc7a53ad
HWangJJUng <marvel94@nhnnext.org>
2015-05-31 18:54:04 +0900

    guildOverviewPage Biz logic

--- webapp/scripts/guildOverview/ubuntudoGuildOverview.js
+++ webapp/scripts/guildOverview/ubuntudoGuildOverview.js
@@ -6,86 +6,17 @@ ubuntudo.utility = {};
 window.addEventListener("load", function () {
     'use strict';
 
+	var oGuildOverviewManager = new ubuntudo.ui.GuildOverviewManager();
 	var util = ubuntudo.utility;
-	var href = window.location.href;
-	var gid = href.substr(href.lastIndexOf("/")+1);
-	var oGuildDataManager = new ubuntudo.ui.GuildDataManager();
 
+
+	oGuildOverviewManager.addEvents();
 	util.ajax({
 	    "method": "GET",
-	    "uri": "/guild/info/"+ gid,
+	    "uri": "/guild/overview/info",
 	    "param": null,
-	    "callback": oGuildDataManager.setGuildInfo
+	    "callback": oGuildOverviewManager.setGuildInfo
 	});
 
-        // 파티 추가하기 박스를 눌렀을 때
-    document.getElementById("party_add_box_btn").addEventListener("click", inputPartyName);
-
-    //파티 추가하기 박스에서, 추가 버튼을 눌렀을 때
-    document.getElementById("inputPartyName_add_btn").addEventListener("click", addParty);
-
-    //파티 추가하기 박스에서, 취소 버튼을 눌렀을 때
-    document.getElementById("inputPartyName_cancel_btn").addEventListener("click", oGuildDataManager.originalPartyAddBox);
-
-    function inputPartyName() {
-        document.getElementById("party_add_box_btn").style.display = "none";
-        document.getElementById("partyName_input_box_btn").style.display = "block"; 
-    }
-
-    function addParty() {
-        var partyName = document.getElementsByClassName("input_partyName")[0].value;
-        util.ajax({
-            "method": "POST",
-            "uri": "/party",
-            "param": "gid="+gid+"&partyName="+ partyName,
-            "callback": oGuildDataManager.addPartyCallback
-        });
-    }
-
-
-    var joinBtn = document.querySelector(".guild_join_btn");
-    var leaveBtn = document.querySelector(".guild_leave_btn");
-    
-    // 길드 가입하기 버튼 클릭 시
-    // 멤버수 늘려야함 
-    joinBtn.addEventListener("click", function () {
-        var util = ubuntudo.utility;
-        
-        /*test용도 gid, uid*/
-        var gid = 1;
-        var param = "guildId=" + gid;
-        util.ajax({
-            "method": "POST",
-            "uri": "/guild/user",
-            "param": param,
-            "callback": callbackSuccessJoin
-	   });
-    });
-    
-    function callbackSuccessJoin () {
-        joinBtn.style.display = "none";
-        leaveBtn.style.display = "block";
-    }
-    
-    // 길드 탈퇴하기 버튼 클릭 시
-    // 멤버수 한 명 빼야함 
-    leaveBtn.addEventListener("click", function () {
-        var util = ubuntudo.utility;
-        
-        /*test용도 gid, uid*/
-        var gid = 1;
-        var uid = 1;
-        var param = "guildId=" + gid + "&userId=" + uid;
-        util.ajax({
-            "method": "POST",
-            "uri": "/guild/user",
-            "param": param,
-            "callback": callbackSuccessLeave
-	   });
-    });
-    
-    function callbackSuccessLeave () {
-        joinBtn.style.display = "block";
-        leaveBtn.style.display = "none";
-    }
+//Hello Hwang! Your keyboard is awesome!!!
 });
